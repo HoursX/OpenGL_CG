a1805427b2e4e1e85dbba121101b41b39105ca29
Bikteen Jiang <jxjsjianghao@163.com>
2023-02-12 03:24:38 +0800

    🎉 init(GLFW&GLAD): First commit

---
+++ OpenGL_CG/OpenGL_CG/Main.cpp
@@ -0,0 +1,30 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "iostream"
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL_CG", nullptr, nullptr);
+
+	if(window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
